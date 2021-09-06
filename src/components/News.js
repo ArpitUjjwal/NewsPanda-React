68575745eadbb6203ff88690c56a74046c050f3a
Arpit <arpitujjwal1@gmail.com>
2021-09-06 20:41:43 +0530

    theme

--- src/components/News.js
+++ src/components/News.js
@@ -26,68 +26,52 @@ export class News extends Component {
         }
     }
 
-    async componentDidMount(){
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c381a923a7c7466eb12f6d3eefeadac3&page=1&pageSize=${this.props.pageSize}`;
-        this.setState({loading: true})
+    async updateNews() {
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c381a923a7c7466eb12f6d3eefeadac3&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
         this.setState({
-            articles: parsedData.articles, 
+            articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false
         })
     }
 
-    handlePrevClick = async ()=>{
-        console.log("Prev");
-
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c381a923a7c7466eb12f6d3eefeadac3&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading: true})
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        console.log(parsedData);
-        this.setState({
-            page: this.state.page - 1,
-            articles: parsedData.articles,
-            loading: false
-        })
+    async componentDidMount() {
+        this.updateNews();
     }
 
-    handleNextClick = async ()=>{
-        console.log("Next");
-        if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/6))){
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c381a923a7c7466eb12f6d3eefeadac3&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            this.setState({loading: true})
-            let data = await fetch(url);
-            let parsedData = await data.json();
-            console.log(parsedData);
-            this.setState({
-                page: this.state.page + 1,
-                articles: parsedData.articles,
-                loading: false
-            })
-        }
+    handlePrevClick = async () => {
+        this.setState({ page: this.state.page - 1 })
+        this.updateNews();
+    }
+
+    handleNextClick = async () => {
+        this.setState({ page: this.state.page + 1 })
+        this.updateNews();
     }
 
     render() {
         return (
             <div className="container my-3 center">
-                <h1 className="text-center" style={{margin: '35px 0px'}}>NewsPanda - Top Headlines</h1>
-                <hr/>
-                {this.state.loading && <Spinner/>}
-                <div className="row" style={{padding: '10px'}}>
-                {!this.state.loading && this.state.articles.map((element)=>{
-                    return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title?element.title:""} description={element.description?element.description.slice(0,140):""} newsUrl={element.url} imageUrl={element.urlToImage} author={element.author} date={element.publishedAt} name={element.source.name}/>
+                <h1 className="text-center" style={{ margin: '10px 0px', color: '#B00000' }}>NewsPanda - Top Headlines</h1>
+                <h5 className="text-center"><span className="badge rounded-pill" style={{ backgroundColor: '#ffb3b3' }}>NewsPanda / {this.props.category}</span></h5>
+                <hr />
+                {this.state.loading && <Spinner />}
+                <div className="row" style={{ padding: '10px' }}>
+                    {!this.state.loading && this.state.articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description.slice(0, 140) : ""} newsUrl={element.url} imageUrl={element.urlToImage} author={element.author} date={element.publishedAt} name={element.source.name} />
                         </div>
-                })}
+                    })}
                 </div>
-                <hr/>
+                <hr />
                 <div className="container d-flex justify-content-between">
-                    <button disabled={this.state.page<=1}type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr;Previous</button>
-                    <p>Page No. {this.state.page}</p>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                    <button disabled={this.state.page <= 1} type="button" style={{ backgroundColor: '#B00000' }} className="btn text-light rounded-pill" onClick={this.handlePrevClick}>&larr;Previous</button>
+                    <p style={{ color: '#B00000' }}><span className="badge rounded-pill" style={{ backgroundColor: '#ffb3b3' }}>Page No. {this.state.page}</span></p>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" style={{ backgroundColor: '#B00000' }} className="btn text-light rounded-pill" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,19 +4,19 @@ export class NewsItem extends Component {
 
 
     render() {
-        let {title, description, imageUrl , newsUrl, author, date , name} = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, name } = this.props;
         return (
             <div className="my-3">
-                <div className="card">
-                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-dark" style={{left:'85%', zIndex: '1'}}>{name}<span className="visually-hidden">unread messages</span></span>
-                    <img src={imageUrl?imageUrl:"https://images.news18.com/ibnlive/uploads/2021/09/shutterstock_2033764355-163081415716x9.jpg"} className="card-img-top" alt="..."/>
+                <div style={{ backgroundColor: '#fff3f4' }} className="card">
+                    <span className="position-absolute top-0 translate-middle rounded-pill badge" style={{ left: '80%', zIndex: '1', backgroundColor: '#B00000' }}>{name}<span className="visually-hidden">unread messages</span></span>
+                    <img src={imageUrl ? imageUrl : "https://images.news18.com/ibnlive/uploads/2021/09/shutterstock_2033764355-163081415716x9.jpg"} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}...</p>
-                        <p className="card-text"><small className="text-muted">By {author?author:"Anonymous"} on {new Date(date).toGMTString()}</small></p>
-                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-secondary">Read More</a>
+                        <p className="card-text"><small className="text-muted">By {author ? author : "Anonymous"} on {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm text-light rounded-pill" style={{ backgroundColor: '#3366ff' }}>Read More</a>
                     </div>
-                </div> 
+                </div>
             </div>
         )
     }
